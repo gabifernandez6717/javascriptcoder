ae42181c75e65bab8dc6bbd04f89618f4c5e52ce
Gabriel Fernandez <gabito2005usa@gmail.com>
2023-07-06 13:15:45 -0300

    cambios a la calculadora

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-//inicio
-alert('Hola! bienvenido a calculadora coder')
-//variables
-let operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar: 1-suma, 2-resta, 3-multiplicacion 4-division ')
-let resultado=0
+alert ('Hola! Bienvenido a calculadora coder')
+ //variables
+let continuarUsandoCalculadora= true
 //funciones
 const sumar= (a,b)=> Number(prompt('primer sumando'))+ Number (prompt('segundo sumando'))
 const restar= (a,b)=>Number(prompt('ingrese el minuendo'))-Number(prompt('ingrese el sustraendo'))
 const multiplicar= (a,b)=> Number(prompt('primer factor'))*Number(prompt('segundo factor'))
 const dividir= (a,b)=> Number(prompt('ingrese el dividendo'))/Number(prompt('ingrese el divisor'))
-//ciclo do...while
+//ciclo
 do {
-    if (operacion=== 1||2||3||4) {
-        if (operacion ==='1') {resultado=sumar()
-            alert('el resultado es '+ resultado)
-            operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar: 1-suma, 2-resta, 3-multiplicacion 4-division ')
-        }
-        if (operacion==='2') {resultado=restar()
-            alert('el resultado es '+ resultado)
-            operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar: 1-suma, 2-resta, 3-multiplicacion 4-division ')
-        }
-        if (operacion==='3') {resultado=multiplicar()
-            alert('el resultado es '+ resultado)
-            operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar: 1-suma, 2-resta, 3-multiplicacion 4-division ')
-        }
-        if (operacion==='4') {resultado=dividir()
-            alert('el resultado es '+ resultado)
-            operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar: 1-suma, 2-resta, 3-multiplicacion 4-division ')
-        }}}
-while (operacion !== 1||2||3||4)
+    //variables
+    let operacion =prompt ( 'ingrese el numero asignado a la operacion que desea efectuar:  1-suma, 2-resta, 3-multiplicacion 4-division')
+    let resultado=0
+    switch (operacion) {
+        case '1':
+            resultado=sumar()
+            alert('El resultado de su operacion es '+resultado)
+            break;
+        case '2':
+            resultado=restar()
+            alert('El resultado de su operacion es '+resultado)
+            break;
+        case '3':
+            resultado=multiplicar()
+            alert('El resultado de su operacion es '+resultado)
+            break;
+        case '4':
+            resultado=dividir()
+            alert('El resultado de su operacion es '+resultado)
+            break;
+        default:
+            alert ('ingrese un numero valido')
+            break;
+    }
+    continuarUsandoCalculadora=confirm('desea seguir usando la calculadora?')
+} while (continuarUsandoCalculadora);
+
+alert('Vuelva cuando quiera!')
 
