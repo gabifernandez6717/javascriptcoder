32ea8af998fee86ed35b8686553fe9609177dc56
Gabriel Fernandez <gabito2005usa@gmail.com>
2023-07-20 22:13:34 -0300

    segunda preentrega

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+alert('Hola! Bienvenido a mi segunda preentrega')
+
+let desicion= Number(prompt('Escriba lo que desee hacer:\n'+'\n 1- Comprar IPhones'+'\n 2- Usar la calculadora'))
+
+let carritoDeIPhones=[]
+
+const modelosIphones=[
+    {modelo:'IPHONE 8', precio: '$10000'},
+    {modelo:'IPHONE X', precio: '$15000'},
+    {modelo:'IPHONE 11', precio: '$20000'},
+    {modelo:'IPHONE 12', precio: '$30000'},
+    {modelo:'IPHONE 13', precio: '$35000'},
+]
+
+const mostrarModelos = modelosIphones.map((modelos)=>'\n'+'-'+modelos.modelo+' '+modelos.precio)
+
+if (desicion=== 1) {
+    alert('Contamos con estos modelos para la venta: \n'+ mostrarModelos)
+    comprar()
+}
+
+
+//CALCULADORA (PRIMERA PREENTREGA)
+if (desicion=== 2) {
 alert ('Hola! Bienvenido a calculadora coder')
  //variables
 let continuarUsandoCalculadora= true
@@ -53,3 +77,4 @@ do {
     continuarUsandoCalculadora=confirm('desea seguir usando la calculadora?')
 } while (continuarUsandoCalculadora);
 alert('Vuelva cuando quiera!')
+}
