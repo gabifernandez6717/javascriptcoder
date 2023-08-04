2685e9b38c4ff20070c663890cd02e7c4a56aaf6
Gabriel Fernandez <gabito2005usa@gmail.com>
2023-08-03 21:16:35 -0300

    tercera preentrega

---
+++ js/main1.js
@@ -0,0 +1,231 @@
+// Recuperar la información del storage
+const carritoInfo = localStorage.getItem('carritoInfo')
+const precioTotal = localStorage.getItem('precioTotal')
+//variables
+const card =document.querySelector('.card')
+const contenedor = document.querySelector('.contenedor')
+const contenedorCarrito = document.querySelector('.header')
+const carrito = []
+//asignarle el evento click a contenedor
+contenedor.addEventListener('click', (e) => {
+    Swal.fire({
+        icon: 'success',
+        title: '¡Añadido al carrito!',
+        text: 'Gracias por su compra, si desea ver su carrito de compras diríjase hacia la parte superior de la página y clickee el carrito.',
+    })
+    const BTNAniadirCarrito = e.target.classList.contains('btn-primary')
+    if (BTNAniadirCarrito) {
+        const productoId = parseInt(e.target.id)
+        const productoEnCarrito = carrito.find(item => item.id === productoId)
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++
+        } else {
+            const producto = IPhones.find(item => item.id === productoId)
+            carrito.push({ ...producto, cantidad: 1 })
+        }
+        console.log('El cliente añadió al carrito un iPhone ' + e.target.id)
+        console.log(carrito)
+    }
+})
+//asignarle evento click al carrito
+contenedorCarrito.addEventListener('click', (e) => {
+    const BTNCarrito = e.target.classList.contains('carritoCompras')
+    if (BTNCarrito) {
+        verCarrito=confirm('desea ver su carrito de compras?')
+        if (verCarrito){
+            let infoCarrito=''
+            let precioTotal = 0
+            for (const producto of carrito) {
+                infoCarrito += 'Producto: '+producto.nombre+', Precio: $'+producto.precio+', Cantidad: '+producto.cantidad+'\n'
+                precioTotal += producto.precio * producto.cantidad
+            }
+            if (infoCarrito) {
+                infoCarrito += '\nPrecio Total: $'+precioTotal
+            // GUARDAR LA INFO EN STORAGE
+            localStorage.setItem('carritoInfo', infoCarrito)
+            localStorage.setItem('precioTotal', precioTotal)
+            Swal.fire({
+                icon: 'info',
+                title: 'Productos en el carrito:\n' + infoCarrito,
+                text: 'estos son los productos que posee tu carrito de compras',
+                })
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Su carrito de compras está vacío.',
+                    text: 'para añadir un producto al carrito es necesario que clickee en "Añadir al carrito"',
+                    })
+            }
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //asignarle el evento click a contenedor
+// contenedor.addEventListener('click', (e) => {
+
+//     const BTNAniadirCarrito = e.target.classList.contains('btn-primary')
+
+//     if (BTNAniadirCarrito) {
+
+//         const productoId = parseInt(e.target.id)
+
+//         const productoEnCarrito = carrito.find(item => item.id === productoId)
+
+//         if (productoEnCarrito) {
+//             productoEnCarrito.cantidad++
+//         } else {
+//             const producto = IPhones.find(item => item.id === productoId);
+
+//             carrito.push({ ...producto, cantidad: 1 })
+//         }
+
+//         console.log('El cliente añadió al carrito un iPhone ' + e.target.id)
+//         console.log(carrito)
+//     }
+// });
+// const noRepetirEnCarrito =(id)=>{
+
+//     const productorepetido=carrito.some(Iphones=>Iphones.id==id)
+
+//     console.log (productorepetido)
+
+//     if (!productorepetido){
+
+//         const modeloComprado=carrito.filter(Iphones=>{
+
+//             if(id== Iphones.id){
+
+//             }
+
+//             return Iphones.nombre && Iphones.precio
+//         })
+//         carrito.push(modeloComprado)
+
+//         console.log(carrito)
+//     }
+// // }
+// contenedor.addEventListener('click', (e) => {
+//     const BTNAniadirCarrito=e.target.classList.contains('btn-primary')
+
+//     if (BTNAniadirCarrito) {
+
+//         console.log ('el cliente compro un iphone '+e.target.id)
+
+//         noRepetirEnCarrito(e.target.id)
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//local storage
+
+
+
+// const card = document.querySelector('.card');
+// const contenedor = document.querySelector('.contenedor');
+// const contenedorCarrito = document.querySelector('.header');
+
+// const carrito = [];
+
+// contenedor.addEventListener('click', (e) => {
+//     const BTNAniadirCarrito = e.target.classList.contains('btn-primary')
+
+//     if (BTNAniadirCarrito) {
+//         const productoId = parseInt(e.target.id)
+//         const productoEnCarrito = carrito.find(item => item.id === productoId)
+
+//         if (productoEnCarrito) {
+//             productoEnCarrito.cantidad++
+//         } else {
+//             const producto = IPhones.find(item => item.id === productoId);
+//             carrito.push({ ...producto, cantidad: 1 })
+//         }
+
+//         console.log('El cliente añadió al carrito un iPhone ' + e.target.id)
+//         console.log(carrito)
+//     }
+// });
+
+// contenedorCarrito.addEventListener('click', (e) => {
+//     const BTNCarrito = e.target.classList.contains('carritoCompras')
+    
+//     if (BTNCarrito) {
+//         const mostrarCarrito = confirm('¿Desea ver su carrito de compras?')
+        
+//         if (mostrarCarrito) {
+//             let infoCarrito = '';
+//             let precioTotal = 0;
+
+//             for (const producto of carrito) {
+//                 infoCarrito += 'Producto: ' + producto.nombre + ', Precio: $' + producto.precio + ', Cantidad: ' + producto.cantidad + '\n';
+//                 precioTotal += producto.precio * producto.cantidad;
+//             }
+
+//             if (infoCarrito) {
+//                 infoCarrito += '\nPrecio Total: $' + precioTotal;
+
+//                 // Guardar información en localStorage
+//                 localStorage.setItem('carritoInfo', infoCarrito);
+//                 localStorage.setItem('precioTotal', precioTotal);
+
+//                 alert('Productos en el carrito:\n' + infoCarrito);
+//             } else {
+//                 alert('Su carrito de compras está vacío.');
+//             }
+//         }
+//     }
+// });
---
+++ js/stock.js
@@ -0,0 +1,17 @@
+const IPhones = [
+    {
+        nombre:'iphone 8', precio: 300,id: 8, cantidad:1
+    },
+    {
+        nombre:'iphone x', precio: 400,id: 10, cantidad:1
+    },
+    {
+        nombre:'iphone 11', precio: 450,id: 11, cantidad:1
+    },
+    {
+        nombre:'iphone 12', precio: 500,id: 12, cantidad:1
+    },
+    {
+        nombre:'iphone 13', precio: 600,id: 13, cantidad:1
+    }
+]
