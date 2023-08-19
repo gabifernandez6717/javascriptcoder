202a023658609015c5949e0c5aeb308a4789cddd
Gabriel Fernandez <gabito2005usa@gmail.com>
2023-08-19 00:57:24 -0300

    tercera preentrega

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-HOLA! Este proyecto es sobre mi curso de JavaScript en CoderHouse, me esta gustando bastante este lenguaje de programación y pienso adentrarme mas en él # javascriptcoder
+HOLA! Este proyecto es sobre mi curso de JavaScript en CoderHouse.
+
+Este proyecto es una tienda virtual de productos Apple, que permite a los usuarios explorar diferentes modelos de iPhones y agregarlos al carrito de compras. Además, incluye un formulario de contacto para consultas que las envía directamente a mi correo y una sección que muestra una lista de clientes satisfechos por intermedio de una api.
+
+Contenido:
+
+-JavaScript
+-HTML
+-CSS
+
+
+JavaScript:
+
+El archivo main1.js contiene la funcionalidad principal de la tienda:
+
+Recupera información del almacenamiento local (localStorage).
+Administra el carrito de compras.
+Muestra mensajes usando SweetAlert2.
+Actualiza la cantidad de productos en el DOM.
+Consume una API externa para mostrar una lista de clientes satisfechos.
+
+
+HTML:
+
+El archivo index.html define la estructura de la página:
+
+Encabezado fijo con barra de navegación.
+Contenedor principal con tarjetas de productos.
+Sección de clientes satisfechos.
+Formulario de contacto.
+Pie de página.
+
+
+CSS:
+
+El archivo styles.css contiene estilos para toda la página:
+
+Estilos generales para el cuerpo y la posición del encabezado.
+Estilos de la barra de navegación y logotipo.
+Estilos para el formulario de contacto.
+Estilos para las tarjetas de productos y su diseño en cuadrícula.
+Estilos para la sección de clientes satisfechos.
+Estilos para el pie de página.
+
+
+
+
+Tecnologías Utilizadas:
+
+-JavaScript
+-HTML
+-CSS
+-Biblioteca SweetAlert2 para mensajes emergentes
+
+
+
+Funcionalidades Clave:
+
+-Agregar productos al carrito de compras.
+-Actualizar la cantidad de productos en las tarjetas.
+-Mostrar una lista de clientes satisfechos desde una API externa.
+-Enviar consultas a través de un formulario de contacto.
+-Vaciar el carrito de compras.
+-Almacenamiento local para mantener el carrito y la información del cliente.
+
+
+
+
+Uso:
+
+Abre el archivo index.html en tu navegador para acceder a la tienda.
+Explora los productos y agrega los deseado al carrito.
+Verifica el carrito haciendo clic en el icono de carrito en la barra de navegación.
+Puedes vaciar el carrito haciendo clic en el botón "Vaciar Carrito".
+Utiliza el formulario de contacto para enviar consultas.
+Explora la lista de clientes satisfechos en la sección correspondiente.
+
+
+
+Espero que te haya gustado este proyecto, un saludo!
--- js/main1.js
+++ js/main1.js
@@ -4,34 +4,49 @@ const precioTotal = localStorage.getItem('precioTotal')
 //variables
 const card =document.querySelector('.card')
 const contenedor = document.querySelector('.contenedor')
+const cardCantidades = document.querySelectorAll('.card-cantidad')
 const contenedorCarrito = document.querySelector('.header')
 const carrito = []
+const clientesSatisfechosList = document.getElementById('clientesSatisfechos')
+const btnVaciarCarrito = document.getElementById('vaciarCarrito')
 //asignarle el evento click a contenedor
 contenedor.addEventListener('click', (e) => {
-    Swal.fire({
-        icon: 'success',
-        title: '¡Añadido al carrito!',
-        text: 'Gracias por su compra, si desea ver su carrito de compras diríjase hacia la parte superior de la página y clickee el carrito.',
-    })
     const BTNAniadirCarrito = e.target.classList.contains('btn-primary')
     if (BTNAniadirCarrito) {
         const productoId = parseInt(e.target.id)
         const productoEnCarrito = carrito.find(item => item.id === productoId)
+        Swal.fire({
+            icon: 'success',
+            title: '¡Añadido al carrito!',
+            text: 'Gracias por su compra, si desea ver su carrito de compras clickee el carrito.',
+            showConfirmButton: false,
+            timer:3500,
+            timerProgressBar: true,
+        })
         if (productoEnCarrito) {
             productoEnCarrito.cantidad++
         } else {
             const producto = IPhones.find(item => item.id === productoId)
             carrito.push({ ...producto, cantidad: 1 })
         }
-        console.log('El cliente añadió al carrito un iPhone ' + e.target.id)
-        console.log(carrito)
+        actualizarCantidadesEnDOM()
     }
 })
+// Función para actualizar la cantidad de iphones comprados en la tarjeta
+function actualizarCantidadesEnDOM() {
+    carrito.forEach(productoEnCarrito=>{
+        const tarjeta= document.querySelector(`.c${productoEnCarrito.id}`)
+        if (tarjeta){
+            const cantidadElement=tarjeta.querySelector('.card-cantidad')
+            cantidadElement.textContent='Cantidad: '+ productoEnCarrito.cantidad
+        }
+    })
+}
 //asignarle evento click al carrito
 contenedorCarrito.addEventListener('click', (e) => {
     const BTNCarrito = e.target.classList.contains('carritoCompras')
     if (BTNCarrito) {
-        verCarrito=confirm('desea ver su carrito de compras?')
+        const verCarrito=true
         if (verCarrito){
             let infoCarrito=''
             let precioTotal = 0
@@ -46,7 +61,7 @@ contenedorCarrito.addEventListener('click', (e) => {
             localStorage.setItem('precioTotal', precioTotal)
             Swal.fire({
                 icon: 'info',
-                title: 'Productos en el carrito:\n' + infoCarrito,
+                title: 'Productos en el carrito:\n \n -' + infoCarrito,
                 text: 'estos son los productos que posee tu carrito de compras',
                 })
             } else {
@@ -59,3 +74,64 @@ contenedorCarrito.addEventListener('click', (e) => {
         }
     }
 })
+// Evento para vaciar el carrito
+btnVaciarCarrito.addEventListener('click', () => {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: 'Esta acción eliminará todos los productos del carrito',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, vaciar carrito',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        // Vaciar el carrito, actualizar cantidad en DOM, poner que las cantidades de las tarjetas quede en cero, "limpiar" el almacenamiento
+        if (result.isConfirmed) {
+            carrito.length = 0
+            actualizarCantidadesEnDOM()
+            cardCantidades.forEach(cantidadElement => {
+                cantidadElement.textContent = 'Cantidad: 0'
+            })
+            localStorage.removeItem('carritoInfo')
+            localStorage.removeItem('precioTotal')
+            Swal.fire(
+                'Carrito vaciado',
+                'Todos los productos han sido eliminados del carrito',
+                'success'
+            )
+        }
+    })
+})
+//API clientes
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => response.json())
+.then(users => {
+    users.forEach(user => {
+        const listItem = document.createElement('li')
+        listItem.textContent = user.name
+        clientesSatisfechosList.appendChild(listItem)
+    })
+})
+.catch(error => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Algo salió mal.',
+        text: 'Estamos trabajando para solucionarlo.'
+    })
+    console.log('Error al obtener datos:', error)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
